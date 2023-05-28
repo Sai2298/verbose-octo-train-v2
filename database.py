0805e4166f39c3ab7bc3c32a43b0b0c0bac5145a
Sai2298 <131674502+Sai2298@users.noreply.github.com>
2023-05-28 06:23:35 +0000

    Added dynamic pages for jobs

--- database.py
+++ database.py
@@ -23,10 +23,22 @@ with engine.connect() as conn:
 
 def load_jobs_from_db():
   with engine.connect() as conn:
-    result = conn.execute(text("select * from jobs"))
+    result = conn.execute(text(F"select * from jobs"))
     jobs =[]
     
     for row in result.all():
       row=row._mapping
       jobs.append(row)
     return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+      result = conn.execute(
+        text(f"SELECT * FROM jobs WHERE id = :val"),
+        {"val": id}
+      )
+      rows = result.mappings().all()
+      if len(rows) ==0:
+        return None
+      else:
+        return dict(rows[0])
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import engine, load_jobs_from_db
+from database import engine, load_jobs_from_db, load_job_from_db
 from sqlalchemy import text
 
 app = Flask(__name__)
@@ -15,6 +15,12 @@ def hello_world():
   jobs=load_jobs_from_db()
   return render_template("home.html", jobs=jobs)
 
+@app.route("/job/<id>")
+def show_job(id):
+  job=load_job_from_db(id)
+  if not job:
+    return "Not found, 404"
+  return render_template("jobpage.html",job=job)
 
 @app.route("/api/jobs")
 def list_jobs():
@@ -23,4 +29,4 @@ def list_jobs():
 
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+  app.run(host='0.0.0.0', port='8081', debug=True)
