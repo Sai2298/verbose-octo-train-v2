4c6d6f70132b3a867bc12557d72ebb50fafd7c68
Sai2298 <131674502+Sai2298@users.noreply.github.com>
2023-05-28 17:00:58 +0000

    Store application in database and add form to collect information.

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-from flask import Flask, render_template, jsonify
-from database import engine, load_jobs_from_db, load_job_from_db
+from flask import Flask, render_template, jsonify, request
+from database import engine, load_jobs_from_db, load_job_from_db, add_application_to_db
 from sqlalchemy import text
 
 app = Flask(__name__)
-
-print(__name__)
-
-
-
-  
 
 @app.route("/")
 def hello_world():
@@ -19,7 +13,7 @@ def hello_world():
 def show_job(id):
   job=load_job_from_db(id)
   if not job:
-    return "Not found, 404"
+    return "Not found, 404. Oops, try again"
   return render_template("jobpage.html",job=job)
 
 @app.route("/api/jobs")
@@ -27,6 +21,18 @@ def list_jobs():
   jobs=load_jobs_from_db()
   return jsonify(jobs)
 
+@app.route("/job/<id>/apply", methods=["post"])
+def apply_to_job(id):
+  job=load_job_from_db(id)
+  #store this in Db
+  
+  data = request.form
+  add_application_to_db(id, data)
+  #display an acknowledgement
+  
+  
+  return render_template('application_submitted.html',application=data, job=job)
+
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port='8081', debug=True)
+  app.run(host='0.0.0.0',  debug=True)
